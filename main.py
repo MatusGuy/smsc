7f4e982662b7d6527ca47cf65fc4d473e69a1057
MatusGuy <mateu.axe@gmail.com>
2022-05-01 22:19:36 +0100

    script detects errors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # ask for admin
-from sys import executable as exe, argv, exit as die
+from sys import executable as exe, argv, exit as abort
 from ctypes import windll
 
 REQUIRES_ADMIN = False # if planning to make a shortcut in the start menu folder, then disabling this won't make that happen
@@ -17,13 +17,28 @@ If that's not what you want to do, and really need this script, change the REQUI
     windll.shell32.ShellExecuteW(None, 'runas', exe, ' '.join(argv), None, None)
     if not IsAdmin():
         print(adminErrorMsg)
-        die()
+        abort()
 
 from os import system as cmd, path, getcwd as cwd
 from win32com.client import Dispatch
 shell = Dispatch("WScript.Shell") # kind of like an import
 
 DEBUG = False
+
+bcolors = {
+    "HEADER": '\033[95m',
+    "OKBLUE": '\033[94m',
+    "OKCYAN": '\033[96m',
+    "OKGREEN": '\033[92m',
+    "WARNING": '\033[93m',
+    "FAIL": '\033[91m',
+    "ENDC": '\033[0m',
+    "BOLD": '\033[1m',
+    "UNDERLINE": '\033[4m'
+}
+
+def PrintWithColour(color:str,msg:str):
+    print(color+msg+bcolors["ENDC"])
 
 def GetFullPathFromAbbreviatedPath(path:str) -> str:
     if path[0] == ".":
@@ -35,27 +50,41 @@ def FakeBreakpoint(msg:str):
     if DEBUG:
         resp = input(msg+"\ncontinue program? y/n: ")
         if resp == "n":
-            die()
+            abort()
 
-def CreateShortcut(file:str,output:str,desc:str="A file/app.",args:str=""):
+def CreateShortcut(file:str,output:str,args:str=""):
     file = f'"{GetFullPathFromAbbreviatedPath(file)}"'
 
     FakeBreakpoint(f"file: {file}; output: {output}")
 
-    link = shell.CreateShortCut(output)
-    link.TargetPath = file
-    link.WorkingDirectory = path.dirname(output)
-    link.Arguments = args
-    link.save()
+    try:
+        link = shell.CreateShortCut(output)
+        link.TargetPath = file
+        link.WorkingDirectory = path.dirname(output)
+        link.Arguments = args
+        link.save()
+    except Exception as err:
+        PrintWithColour(bcolors["FAIL"],f"ERROR: {err.args[2][2]}")
+        abort()
 
     FakeBreakpoint(f"link: {link}")
 
     return link
 
-print(argv)
+argv = [" "," "," "]
+
+if len(argv) < 3:
+    PrintWithColour(bcolors["FAIL"],"ERROR: Insufficient arguments")
+    abort()
+
+lnkArgs = ""
+try:
+    lnkArgs = argv[3]
+except IndexError:
+    PrintWithColour(bcolors["WARNING"],"WARNING: Shortcut args unspecified.")
+
 CreateShortcut(
     argv[1],
     argv[2],
-    argv[3],
-    argv[4]
+    lnkArgs
 )
