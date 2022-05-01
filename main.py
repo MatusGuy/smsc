72ca6979d6199ca0afbd51ca854ea5efa7decf1f
MatusGuy <mateu.axe@gmail.com>
2022-05-01 20:20:32 +0100

    switched to pywin32

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 # ask for admin
 from sys import executable as exe, argv
 from ctypes import windll
-windll.shell32.ShellExecuteW(None, 'runas', exe, ' '.join(argv), None, None)
+#windll.shell32.ShellExecuteW(None, 'runas', exe, ' '.join(argv), None, None)
 
-from os import system as cmd
-from winshell import shortcut as mklink,Shortcut
+from os import system as cmd, path, getcwd as cwd
+from win32com.client import Dispatch
+shell = Dispatch("WScript.Shell") # kind of like an import
 
-def CreateShortcut(file:str,output:str,desc:str="A file/app.",args:str="") -> Shortcut:
-    with mklink(output) as link:
-        link.path = file
-        link.description = desc
-        link.arguments = args
-        return link
+def GetFullPathFromAbbreviatedPath(path:str) -> str:
+    if path[0] == ".":
+        newpath = path.replace(".",cwd(),1)
+    
+    return newpath
 
+def CreateShortcut(file:str,output:str,desc:str="A file/app.",args:str=""):
+    link = shell.CreateShortCut(output)
+    link.Targetpath = file
+    link.WorkingDirectory = path.dirname(output)
+    link.Arguments = args
+    return link
+
+print(argv)
 CreateShortcut(
     argv[1],
     argv[2],
